59fd79d9d9a7b1ca2b42863adcae31fb46583942
Raju George <test@example.com>
2023-12-12 09:41:21 +0530

    candidate education select box issue fixed

--- app/(admin)/admin/(main)/candidates/[id]/components/valueeditplusttwoform.tsx
+++ app/(admin)/admin/(main)/candidates/[id]/components/valueeditplusttwoform.tsx
@@ -39,6 +39,7 @@ const ValueEditPlusTwoForm = ({ candidateId, value, setEditMode }) => {
     control,
     clearErrors,
     setError,
+    setValue,
     formState: { errors, isSubmitting },
   } = useForm({
     resolver: async (data, context, options) => {
@@ -88,6 +89,7 @@ const ValueEditPlusTwoForm = ({ candidateId, value, setEditMode }) => {
                   defaultValue={stateId}
                   onChange={(e) => {
                     setStateId(e.target.value);
+                    setValue("otherState", "");
                     field.onChange(e.target.value);
                   }}
                   className="mt-2 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-pink-600 sm:text-sm sm:leading-6"
