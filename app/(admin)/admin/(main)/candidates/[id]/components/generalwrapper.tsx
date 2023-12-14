e6883930934dffea1b3f5fa4145ccdeaad4d00aa
Raju George <test@example.com>
2023-12-14 13:45:30 +0530

    fixing photo issues

--- app/(admin)/admin/(main)/candidates/[id]/components/generalwrapper.tsx
+++ app/(admin)/admin/(main)/candidates/[id]/components/generalwrapper.tsx
@@ -9,7 +9,7 @@ import PlusTwo from "./plustwo";
 const GeneralWrapper = ({ candidateId }) => {
   return (
     <div className="mx-auto max-w-2xl space-y-16 sm:space-y-20 lg:mx-0 lg:max-w-none">
-      <PersonalInfo candidateId={candidateId} />
+      {/* <PersonalInfo candidateId={candidateId} /> */}
       <ContactAddress candidateId={candidateId} />
       <ParentInfo candidateId={candidateId} />
       <PlusTwo candidateId={candidateId} />
