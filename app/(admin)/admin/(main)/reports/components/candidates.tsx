4f6ae457a6a3b1300d715a534197ca41bb346f1d
gokulnfonics <gokul@nfonics.com>
2023-12-28 13:02:40 +0530

    adding changes

--- app/(admin)/admin/(main)/reports/components/candidates.tsx
+++ app/(admin)/admin/(main)/reports/components/candidates.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { getAllCandidatesInfo } from "@/app/data/reports/candidate";
+import { getAllCandidatesInfo, getSignedUpCandidates, getProfileCompleted, getAppliedReport } from "@/app/data/reports/candidate";
 import { ArrowDownOnSquareIcon } from "@heroicons/react/24/outline";
 import React, { useState } from "react";
 import FileDownload from "js-file-download";
@@ -7,21 +7,48 @@ import DataLoader from "@/app/components/DataLoader";
 
 const Candidates = () => {
   const [isFetching, setIsFetching] = useState(false);
+  const [selectedReport, setSelectedReport] = useState("allCandidates");
 
-  async function handleExcelDownload() {
+  const reportOptions = [
+    { value: "signedUp", label: "Signed Up" },
+    { value: "profileCompleted", label: "Profile Completed" },
+    { value: "applied", label: "Applied" },
+  ];
+
+  const handleExcelDownload = async () => {
     setIsFetching(true);
+
     try {
-      const data = await getAllCandidatesInfo();
+      let data;
 
-      const filename = `All-Candidates-${Date.now()}.xlsx`;
+      if (selectedReport === "allCandidates") {
+        data = await getAllCandidatesInfo();
+      } else {
+        switch (selectedReport) {
+          case "signedUp":
+            data = await getSignedUpCandidates();
+            break;
+          case "profileCompleted":
+            data = await getProfileCompleted();
+            break;
+          case "applied":
+            data = await getAppliedReport();
+            break;
+          default:
+            break;
+        }
+      }
 
+      const filename = `${selectedReport}-Report-${Date.now()}.xlsx`;
       FileDownload(data, filename);
       setIsFetching(false);
     } catch (error) {
       setIsFetching(false);
       console.error("Error downloading Excel file:", error);
     }
-  }
+  };
+
+
   return (
     <div className="px-4 sm:px-6 lg:px-8">
       <div className="space-y-5">
@@ -30,6 +57,22 @@ const Candidates = () => {
             Candidates Reports
           </h2>
           <p className="text-sm">Download details of all the candidates</p>
+        </div>
+        <div className="grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+          <div className="sm:col-span-2">
+          <select
+            value={selectedReport}
+            onChange={(e) => setSelectedReport(e.target.value)}
+            className="mt-2 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-pink-600 sm:text-sm sm:leading-6"
+          >
+            <option value="allCandidates">All Candidates</option>
+            {reportOptions.map((option) => (
+              <option key={option.value} value={option.value}>
+                {option.label}
+              </option>
+            ))}
+          </select>
+          </div>
         </div>
         <div>
           <button
