4c959322fdb518be7326954cd022b230f34ea899
Raju George <test@example.com>
2023-12-29 11:05:39 +0530

    phase details v1

---
+++ app/(admin)/admin/(main)/master/components/phasedetails.tsx
@@ -0,0 +1,48 @@
+import PhaseDateEdit from "./phasedateedit";
+import PhaseNoEdit from "./phasenoedit";
+import PhaseSlotEdit from "./phaseslotedit";
+
+export const PhaseDetails = ({ exam, refetchData }) => {
+  // show default details
+  // update details
+  return (
+    <>
+      <div className="px-4 sm:px-0">
+        <h3 className="text-base font-semibold leading-7 text-gray-900">
+          Phase Details
+        </h3>
+        <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-500">
+          Phase details . Leave or clear Registration End Date, if not decided.
+        </p>
+      </div>
+      <div className="mt-6 border-t border-gray-100">
+        <dl className="divide-y divide-gray-100">
+          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm font-medium leading-6 text-gray-900">
+              Phase No.
+            </dt>
+            <dd className="mt-1 flex text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+              <PhaseNoEdit exam={exam} refetchData={refetchData} />
+            </dd>
+          </div>
+          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm font-medium leading-6 text-gray-900">
+              Slot Registration Closes
+            </dt>
+            <dd className="mt-1 flex text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+              <PhaseDateEdit exam={exam} />
+            </dd>
+          </div>
+          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm font-medium leading-6 text-gray-900">
+              Slot Booking
+            </dt>
+            <dd className="mt-1 flex text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+              <PhaseSlotEdit exam={exam} refetchData={refetchData} />
+            </dd>
+          </div>
+        </dl>
+      </div>
+    </>
+  );
+};
