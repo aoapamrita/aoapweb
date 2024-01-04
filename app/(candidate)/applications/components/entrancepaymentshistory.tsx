8bc68e631f57ca5f3739304f95440bc32325d00d
gokulnfonics <gokul@nfonics.com>
2024-01-04 13:54:41 +0530

    Completed PDF Download of the payment receipt

--- app/(candidate)/applications/components/entrancepaymentshistory.tsx
+++ app/(candidate)/applications/components/entrancepaymentshistory.tsx
@@ -1,28 +1,40 @@
 import React from "react";
 import TransactionBadge from "./transactionbadge";
 import dayjs from "dayjs";
+import getCandidate from "@/app/data/getCandidate";
+import {  useQuery } from "@tanstack/react-query";
+import numberToWords from 'number-to-words';
 import "../../../style/receiptstyle.css";
-import { ArrowDownOnSquareIcon } from "@heroicons/react/24/outline";
-
 
 const EntrancePaymentsHistory = ({ transactions }) => {
-  console.log(transactions);
 
+  const { data: candidate } = useQuery({
+    queryKey: ["candidate"],
+    queryFn: () => getCandidate(),
+  });
+  
+  { /*FEE Receipt PDF download function */}
   const downloadPDF = () => {
-    const element = document.getElementById('fee-receipt');
-    // element.style.display = 'block';
+    const element = document.getElementById('hidden-fee-receipt');
+    element.style.display = 'block';
     import('html2pdf.js').then(({ default: html2pdf }) => {
       html2pdf(element, {
         margin: 1,
         filename: 'FEE-RECEIPT.pdf',
         image: { type: 'jpeg', quality: 0.98 },
-        html2canvas: { scale: 1, useCORS: true },
-        jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' },
+        html2canvas: { scale: 2, useCORS: true },
+        jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait'},
       }).then(() => {
-        // element.style.display = 'none';
+        element.style.display = 'none';
       });
     });
   };
+
+  {/* Amount to words */}
+  let amountInWords = "N/A";  
+    if (transactions && transactions.length > 0 && transactions[0].amount) {
+      amountInWords = numberToWords.toWords(transactions[0].amount).toUpperCase();
+    }
 
   return (
     <div>
@@ -66,6 +78,11 @@ const EntrancePaymentsHistory = ({ transactions }) => {
                       >
                         Date
                       </th>
+                      <th
+                        scope="col"
+                        className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"> 
+                        Receipt
+                      </th>
                     </tr>
                   </thead>
                   <tbody className="divide-y divide-gray-200 bg-white">
@@ -86,6 +103,14 @@ const EntrancePaymentsHistory = ({ transactions }) => {
                             "DD/MM/YYYY hh:mm:ss"
                           )}
                         </td>
+                        <td>
+                          <button
+                            onClick={downloadPDF}
+                            className="text-sm font-medium text-pink-600 hover:text-pink-500"
+                          >
+                            Download 
+                          </button>                  
+                      </td>
                       </tr>
                     ))}
                   </tbody>
@@ -96,8 +121,9 @@ const EntrancePaymentsHistory = ({ transactions }) => {
         </div>
       ) : null}
 
-      <div className="fee-receipt a4-div" id="fee-receipt">
-
+      {/*FEE Receipt PDF design */}       
+      <div className="hidden" id="hidden-fee-receipt">
+      <div className="fee-receipt a4-paper" id="fee-receipt">
         <div className="head-section">
           <div className="logo">
             <img 
@@ -105,31 +131,30 @@ const EntrancePaymentsHistory = ({ transactions }) => {
                   width="200"
                   height="100"
                   alt="Amrita Vishwa Vidyapeetham"
-                /> 
+            /> 
           </div>
-          <div className="font-bold">
-            <p>Directorate of Admissions, AMRITA VISHWA VIDYAPEETHAM,</p>
-            <p>AMRITA NAGAR (PO), ETTIMADAI, COIMBATORE-641112 Tamilnadu</p>
+          <div className="font-medium pb-2">
+            <p>DIRECTORATE OF ADMISSIONS, AMRITA VISHWA VIDYAPEETHAM,</p>
+            <p>AMRITA NAGAR (PO), ETTIMADAI, COIMBATORE-641112, TAMILNADU</p>
           </div>
         </div>
 
         <div className="flex justify-between items-start">
           <h3 className="font-bold text-lg ml-3">APPLICATION FEE RECEIPT</h3>
           <div className="receipt-date">
-            <p><span className="date-span">Receipt Date</span>: 21/12/2023 </p>
-            <p><span className="date-span">Receipt No</span>: 123456</p>
+            <p>Receipt Date : {dayjs(transactions.updatedAt).format("DD/MM/YYYY")} </p>
           </div>
         </div>
         
         <div className="receipt-details">
-            <h3 className="font-bold mt-10 pt-5 ml-3"> Recipient Details</h3>
+            <h3 className="font-medium mt-10 pt-5 ml-3 w-72"> Recipient Details</h3>
               <div className="receipt-address">
-                <p><span className="add-span">Registration Number</span>:10000</p>
-                <p>SOORYA DAS</p>
-                <p>SOORYA VILLA</p>
-                <p>LAKKIDI KOOTTUPATHA, OTTAPPALAM, PALAKKAD, KERALA</p>
-                <p><span className="add-span">Email</span>:soory@gmail.com</p>
-                <p><span className="add-span">Phone</span>:9999999999</p>
+                <p>Application Number : {transactions[0]?.examapplication?.reference}</p>
+                <p>{candidate?.fullname}</p>
+                <p>{candidate?.address1}</p>
+                <p>{candidate?.address2}</p>
+                <p><span className="sp-100">Email </span>: {candidate?.email}</p>
+                <p><span className="sp-100">Phone </span>: {candidate?.phone}</p>
               </div>
         </div>
 
@@ -143,34 +168,36 @@ const EntrancePaymentsHistory = ({ transactions }) => {
                 <th className="head-data">Transaction Date</th>
               </tr>
             </thead>
-            <tbody className="table-body">
-              <tr className="table-row">
-                <td className="table-data"> Online </td>
-                <td className="table-data"> AEEE-1F64-CLQW-1704182437915 </td>
-                <td className="table-data tb"> INR 1400.00</td>
-                <td className="table-data"> 14/02/2023 </td>
-              </tr>
+            <tbody className="table-body">             
+            {transactions.map((transaction) => (
+                      <tr key={transaction.id}>
+                        <td className="table-data">
+                          <p>{transaction.type}</p>
+                        </td>
+                        <td className="table-data">
+                          <p>{transaction.reference}</p>
+                        </td>
+                        <td className="table-data tb">
+                          <p>{transaction.amount}/-</p>
+                        </td>
+                        <td className="table-data">
+                          {dayjs(transaction.updatedAt).format(
+                            "DD/MM/YYYY hh:mm:ss"
+                          )}
+                        </td>
+                      </tr>
+                    ))}
             </tbody>
           </table>
-          <p className="font-bold text-right px-3 py-4">AMOUNT IN WORDS : RUPEES ONE THOUSAND FOUR HUNDRED ONLY</p>
 
+          <p className="font-medium text-right px-3 py-4">AMOUNT IN WORDS : RUPEES {amountInWords} ONLY</p>
           <p className="font-bold text-right px-3 py-4">Directorate of Admissions</p>
         </div>
-
       </div>
-
-      <button
-                      type="button"
-                      className="flex gap-1 items-center rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-                      onClick={downloadPDF}
-                    >
-                      <ArrowDownOnSquareIcon className="h-6 w-6"  />
-                      Download FEE RECEIPT
-                    </button>
-
-
+      </div>
     </div>
   );
+  
 };
 
 export default EntrancePaymentsHistory;
