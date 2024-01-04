f0136cec593c9ae0175566d38ccdf0eba46f89ea
Raju George <test@example.com>
2024-01-04 21:09:44 +0530

    sync slot issues fixed

--- app/(admin)/admin/(main)/vendors/usersync/components/listusers.tsx
+++ app/(admin)/admin/(main)/vendors/usersync/components/listusers.tsx
@@ -11,62 +11,66 @@ const ListUsers = ({ examId }) => {
   });
 
   return (
-    <div>
-      {isLoading ? (
-        <DataLoader />
-      ) : (
-        <table className="min-w-full divide-y divide-gray-300">
-          <thead>
-            <tr>
-              <th
-                scope="col"
-                className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
-              >
-                Name
-              </th>
-              <th
-                scope="col"
-                className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
-              >
-                Phone
-              </th>
-              <th
-                scope="col"
-                className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 lg:table-cell"
-              >
-                Reg No.
-              </th>
-              <th
-                scope="col"
-                className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 lg:table-cell"
-              >
-                Status
-              </th>
-              <th
-                scope="col"
-                className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 lg:table-cell"
-              >
-                Comment
-              </th>
+    <>
+      <div>
+        <div>
+          {!registrations ? (
+            <DataLoader />
+          ) : (
+            <table className="min-w-full divide-y divide-gray-300">
+              <thead>
+                <tr>
+                  <th
+                    scope="col"
+                    className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
+                  >
+                    Name
+                  </th>
+                  <th
+                    scope="col"
+                    className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
+                  >
+                    Phone
+                  </th>
+                  <th
+                    scope="col"
+                    className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 lg:table-cell"
+                  >
+                    Reg No.
+                  </th>
+                  <th
+                    scope="col"
+                    className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 lg:table-cell"
+                  >
+                    Status
+                  </th>
+                  <th
+                    scope="col"
+                    className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 lg:table-cell"
+                  >
+                    Comment
+                  </th>
 
-              <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-6">
-                <span className="sr-only">Verify</span>
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {registrations.map((registration, rowIdx) => (
-              <UserRow
-                key={registration.id}
-                registration={registration}
-                rowId={rowIdx}
-                examId={examId}
-              />
-            ))}
-          </tbody>
-        </table>
-      )}
-    </div>
+                  <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-6">
+                    <span className="sr-only">Verify</span>
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {registrations.map((registration, rowIdx) => (
+                  <UserRow
+                    key={registration.id}
+                    registration={registration}
+                    rowId={rowIdx}
+                    examId={examId}
+                  />
+                ))}
+              </tbody>
+            </table>
+          )}
+        </div>
+      </div>
+    </>
   );
 };
 
